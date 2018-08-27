b9b40b5e0608afbcbdfcf9d3f5a6fc21111cfa1c
p8ryjr114 <42733834+p8ryjr114@users.noreply.github.com>
2018-08-27 10:55:03 +0200

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,12 @@
-# Hello
-// This code displays Hello-world with abstraction
-// This commit adds onto the readme description
+                    # Hello
+                    // This code displays Hello-world with abstraction
+                    // This commit adds onto the readme description
 
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package hello;
 import java.util.Scanner;
 /**
@@ -16,9 +21,9 @@ public class Hello {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         HelloWater water = new HelloWater();
-        HelloWeather weather = new HelloWeather();
-        System.out.println(water.Show());
-        System.out.println(weather.Show());
+        HelloWind wind = new HelloWind();
+        System.out.println("You see some water and say " + water.Show());
+        System.out.println("You feel the wind and shout " + wind.Show() + "!!");
     }
     
 }
@@ -34,11 +39,11 @@ class HelloWater extends HelloW{
     }
 
 }
-class HelloWeather extends HelloW{
+class HelloWind extends HelloW{
 
     @Override
     public String Show() {
-        return("HelloWeather");
+        return("HelloWind");
     }
 
 }
